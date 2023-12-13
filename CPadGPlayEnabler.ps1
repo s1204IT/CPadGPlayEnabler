f7aa8af91792b65ba19bf54e074aed529102b7dd
Syuugo <pub@s1204.me>
2023-12-13 13:49:04 +0900

    CTZにGoogleサービスを追加

--- CPadGPlayEnabler.ps1
+++ CPadGPlayEnabler.ps1
@@ -39,7 +39,8 @@ function GrantApp ($AppName, $AppPackage, $perms) {
 }
 
 # 端末識別
-switch ("$(adb shell getprop ro.product.model)") {
+$CPadModel = $(adb shell getprop ro.product.model)
+switch ($CPadModel) {
     ({"TAB-A03-B[S,R]" -or "TAB-A03-BR2"}) {
         ExistCPad "２"
         $CT2 = 1
@@ -50,7 +51,10 @@ switch ("$(adb shell getprop ro.product.model)") {
         Write-Output "`r`nCPadGAppsが利用出来ます`r`n"
     }
     "TAB-A05-BD" { ExistCPad "Neo" }
-    "TAB-A05-BA1" { ExistCPad "Next" }
+    "TAB-A05-BA1" {
+        ExistCPad "Next"
+        $CTZ = 1
+    }
     default {
         Write-Output "チャレンジパッドが検出されませんでした"
         Read-Host "もう一度やり直して下さい｡(Enter)"
@@ -62,40 +66,82 @@ switch ("$(adb shell getprop ro.product.model)") {
 Read-Host "続行しますか？(Enter)"
 Clear-Host
 
+if ($CTZ -eq 1) {
+    adb shell cat /factory/count_dcha_completed | Out-Null
+    if ($? -ne $true) {
+        Write-Output "DchaState が 3 になった痕跡が見つかりませんでした"
+        Write-Output "Google Playストア を使用するには DchaState を 3 にする必要があります `r`n"
+        Write-Output "Play ストア を利用する場合は｢y｣と入力してEnter､`r`nmicroGを利用する場合は何も入力せずEnterを押して下さい`r`n"
+        $confirm = Read-Host "Google Playストアを利用しますか？"
+        if ($confirm -ne "y") {
+            $CTZ = 0
+        } else {
+            adb shell settings put system dcha_state 3
+        }
+    }
+    Clear-Host
+}
 
 # Googleサービスのインストール
 
 if ($CT2 -eq 1) {
     
     # Googleアカウントマネージャー
-    InstApp "Googleアカウントマネージャー" GoogleLoginService_22
+    InstApp "Googleアカウントマネージャー" CT2\GoogleLoginService
     GrantApp "Googleアカウントマネージャー" com.google.android.gsf.login DUMP,READ_LOGS
 
     # Googleサービスフレームワーク
-    InstApp "Googleサービスフレームワーク" GoogleServicesFramework_19
+    InstApp "Googleサービスフレームワーク" CT2\GoogleServicesFramework
     GrantApp "Googleサービスフレームワーク" com.google.android.gsf DUMP,READ_LOGS,WRITE_SECURE_SETTINGS,INTERACT_ACROSS_USERS
 
     # Google Play開発者サービス
-    InstApp "Google Play開発者サービス" GmsCore_225014006
+    InstApp "Google Play開発者サービス" CT2\GmsCore
     GrantApp "Google Play開発者サービス" com.google.android.gms INTERACT_ACROSS_USERS,PACKAGE_USAGE_STATS,GET_APP_OPS_STATS,READ_LOGS
     adb shell dpm set-active-admin --user 0 com.google.android.gms/.mdm.receivers.MdmDeviceAdminReceiver | Out-Null
 
     # Google Playストア
-    InstApp "Google Playストア" Phonesky_82791710
+    InstApp "Google Playストア" CT2\Phonesky
     GrantApp "Google Playストア" com.android.vending PACKAGE_USAGE_STATS,BATTERY_STATS,DUMP,GET_APP_OPS_STATS,INTERACT_ACROSS_USERS,WRITE_SECURE_SETTINGS
 
+} elseif ($CTZ -eq 1) {
+    #
+    # CTZ は権限付与が不要
+    #
+
+    # 整備
+    InstApp "DchaState Receiver" CTZ\DchaStateReceiver
+    #adb shell appops set me.s1204.benesse.dcha.receiver WRITE_SETTINGS allow
+    adb shell dpm set-active-admin --user 0 me.s1204.benesse.dcha.receiver/.DeviceAdminReceiver | Out-Null
+    
+    # Googleサービスフレームワーク
+    InstApp "Googleサービスフレームワーク" CTZ\GoogleServicesFramework
+
+    # Google Play開発者サービス
+    InstApp "Google Play開発者サービス" CTZ\GmsCore
+    # これだけ強制
+    adb shell dumpsys deviceidle whitelist +"com.google.android.gms" | Out-Null
+
+    # Google Playストア
+    InstApp "Google Playストア" CTZ\Phonesky
+
+    # Googleの連絡先の同期
+    InstApp "Googleの連絡先の同期" CTZ\GoogleContactsSyncAdapter
+
+    # Googleカレンダーの同期
+    InstApp "Googleカレンダーの同期" CTZ\GoogleCalendarSyncAdapter
+    
 } else {
 
     # microG Services Framework Proxy
     InstApp "microG Services Framework Proxy" microG\GsfProxy
 
-    # microG Services Core
-    InstApp "microG Services Core" microG\com.google.android.gms-231657056
-    GrantApp "microG Services Core" com.google.android.gms ACCESS_COARSE_LOCATION,ACCESS_FINE_LOCATION,READ_PHONE_STATE,GET_ACCOUNTS,WRITE_EXTERNAL_STORAGE,READ_EXTERNAL_STORAGE,RECEIVE_SMS,SYSTEM_ALERT_WINDOW,BODY_SENSORS
+    # microG Services
+    InstApp "microG Services" microG\com.google.android.gms-233515000
+    GrantApp "microG Services" com.google.android.gms ACCESS_COARSE_LOCATION,ACCESS_FINE_LOCATION,READ_PHONE_STATE,GET_ACCOUNTS,WRITE_EXTERNAL_STORAGE,READ_EXTERNAL_STORAGE,RECEIVE_SMS,SYSTEM_ALERT_WINDOW,BODY_SENSORS
     adb shell dumpsys deviceidle whitelist +"com.google.android.gms" | Out-Null
 
-    # FakeStore
-    InstApp "FakeStore" microG\com.android.vending-30
+    # microG Companion
+    InstApp "microG Companion" microG\com.android.vending-83752400
 
 }
 Clear-Host
