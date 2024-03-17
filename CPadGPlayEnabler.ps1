90e2717e6da294af74cdc0af1ac84d94f43ff098
Syuugo <pub@s1204.me>
2024-03-17 14:34:45 +0900

    いろいろ更新

--- CPadGPlayEnabler.ps1
+++ CPadGPlayEnabler.ps1
@@ -28,7 +28,7 @@ function ExistCPad ($CPadType) {
 
 function InstApp ($AppName, $AppPackage) {
     Write-Output "｢$AppName｣をインストール中..."
-    adb install -r .\assets\$AppPackage.apk | Out-Null
+    adb install -r ".\assets\$AppPackage.apk" | Out-Null
 }
 
 function GrantApp ($AppName, $AppPackage, $perms) {
@@ -42,15 +42,22 @@ function GrantApp ($AppName, $AppPackage, $perms) {
 switch ("$(adb shell getprop ro.product.model)") {
     ({"TAB-A03-B[S,R]" -or "TAB-A03-BR2"}) {
         ExistCPad "２"
-        $CT2 = 1
+        $PType = '2'
     }
     "TAB-A03-BR3" {
         ExistCPad "３"
-        Start-Process "https://github.com/s1204IT/CPadGApps/tree/CT3"
+        Start-Process "https://zenn.dev/s1204it/articles/16fce85441821f"
         Write-Output "`r`nCPadGAppsが利用出来ます`r`n"
+        $PType = '3'
     }
-    "TAB-A05-BD" { ExistCPad "Neo" }
-    "TAB-A05-BA1" { ExistCPad "Next" }
+    "TAB-A05-BD" {
+        ExistCPad "Neo"
+        $PType = 'X'
+    }
+    "TAB-A05-BA1" {
+        ExistCPad "Next"
+        $PType = 'Z'
+    }
     default {
         Write-Output "チャレンジパッドが検出されませんでした"
         Read-Host "もう一度やり直して下さい｡(Enter)"
@@ -62,42 +69,52 @@ switch ("$(adb shell getprop ro.product.model)") {
 Read-Host "続行しますか？(Enter)"
 Clear-Host
 
+# CTZ の場合のみ DchaState を 3 に修正する
+if ($Ptype -eq "Z") {
+    adb shell settings put system dcha_state 3
+    InstApp "Bypass Revoke Permission" BypassRevokePermission
+}
 
 # Googleサービスのインストール
 
-if ($CT2 -eq 1) {
-    
-    # Googleアカウントマネージャー
-    InstApp "Googleアカウントマネージャー" GoogleLoginService_22
-    GrantApp "Googleアカウントマネージャー" com.google.android.gsf.login DUMP,READ_LOGS
+if ({$PType -eq "2" -or $PType -eq "Z"}) {
 
     # Googleサービスフレームワーク
-    InstApp "Googleサービスフレームワーク" GoogleServicesFramework_19
+    InstApp "Googleサービスフレームワーク" "CT$PType\GoogleServicesFramework"
     GrantApp "Googleサービスフレームワーク" com.google.android.gsf DUMP,READ_LOGS,WRITE_SECURE_SETTINGS,INTERACT_ACROSS_USERS
 
     # Google Play開発者サービス
-    InstApp "Google Play開発者サービス" GmsCore_225014006
+    InstApp "Google Play開発者サービス" "CT$PType\GmsCore"
     GrantApp "Google Play開発者サービス" com.google.android.gms INTERACT_ACROSS_USERS,PACKAGE_USAGE_STATS,GET_APP_OPS_STATS,READ_LOGS
     adb shell dpm set-active-admin --user 0 com.google.android.gms/.mdm.receivers.MdmDeviceAdminReceiver | Out-Null
 
     # Google Playストア
-    InstApp "Google Playストア" Phonesky_82791710
+    InstApp "Google Playストア" "CT$PType\Phonesky"
     GrantApp "Google Playストア" com.android.vending PACKAGE_USAGE_STATS,BATTERY_STATS,DUMP,GET_APP_OPS_STATS,INTERACT_ACROSS_USERS,WRITE_SECURE_SETTINGS
 
 } else {
 
-    # microG Services Framework Proxy
-    InstApp "microG Services Framework Proxy" microG\GsfProxy
+    # Services Framework Proxy
+    InstApp "microG Services Framework Proxy" microG\ServicesFrameworkProxy
 
-    # microG Services Core
-    InstApp "microG Services Core" microG\com.google.android.gms-231657056
-    GrantApp "microG Services Core" com.google.android.gms ACCESS_COARSE_LOCATION,ACCESS_FINE_LOCATION,READ_PHONE_STATE,GET_ACCOUNTS,WRITE_EXTERNAL_STORAGE,READ_EXTERNAL_STORAGE,RECEIVE_SMS,SYSTEM_ALERT_WINDOW,BODY_SENSORS
+    # microG Services
+    InstApp "microG Services" microG\microGServices
+    GrantApp "microG Services" com.google.android.gms ACCESS_COARSE_LOCATION,ACCESS_FINE_LOCATION,READ_PHONE_STATE,GET_ACCOUNTS,WRITE_EXTERNAL_STORAGE,READ_EXTERNAL_STORAGE,RECEIVE_SMS,SYSTEM_ALERT_WINDOW,BODY_SENSORS
     adb shell dumpsys deviceidle whitelist +"com.google.android.gms" | Out-Null
 
-    # FakeStore
-    InstApp "FakeStore" microG\com.android.vending-30
+    # microG Companion
+    InstApp "microG Companion" microG\microGCompanion
 
 }
+
+# CT2 は GMS の権限を修正
+if ($PType -eq "2") {
+    InstApp "Enable GServices" EnableGServices
+    adb shell am start -n com.saradabar.enablegservices/.MainActivity
+    Start-Sleep 3
+    adb shell pm uninstall com.saradabar.enablegservices | Out-Null
+}
+
 Clear-Host
 
 # 端末を再起動
